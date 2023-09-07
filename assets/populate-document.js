ddb9dad05a6b9d3ff454baa910661cd6d2fb926e
thatjuancodes <thejuan.codes@gmail.com>
2023-09-08 00:41:59 +0800

    Fix dynamic fields for all forms

--- assets/populate-document.js
+++ assets/populate-document.js
@@ -1,25 +1,29 @@
-window.onload = function () {
-  const documentParent = document.getElementById("document-parent");
-  const documentActual = document.getElementById("document-actual");
-  const documentEditable = documentActual.cloneNode(true);
+document.onreadystatechange = function () {
+  if (document.readyState === "interactive") {
+    const documentParent = document.getElementById("document-parent");
+    const documentActual = document.getElementById("document-actual");
+    const documentEditable = documentActual.cloneNode(true);
+    documentEditable.id = "document-editable";
 
-  const fields = documentEditable.getElementsByClassName("actual-field");
-  const fieldsArray = [].slice.call(fields);
+    const fields = documentEditable.getElementsByClassName("actual-field");
+    const fieldsArray = [].slice.call(fields);
 
-  documentParent.prepend(documentEditable);
+    documentParent.prepend(documentEditable);
 
-  documentEditable.getElementsByClassName("view-title")[0].innerHTML = "Edit View";
-  documentEditable.getElementsByClassName("view-title")[0].className = "view-title text-center segment-title text-secondary mt-4";
+    documentEditable.getElementsByClassName("view-title")[0].innerHTML = "Edit View";
+    documentEditable.getElementsByClassName("view-title")[0].className = "view-title text-center segment-title text-secondary mt-4";
 
-  fieldsArray.forEach(function (field, index) {
-    const input = document.createElement("input");
-    input.type = "text";
-    input.placeholder = field.id;
-    input.id = field.id + "-editable";
-    input.className = field.className + " pseudo-field";
-    input.addEventListener('keyup', function (e) {
-      changeField(input.id, field.id)
+    fieldsArray.forEach(function (field, index) {
+      const input = document.createElement("input");
+      input.type = "text";
+      input.placeholder = field.id;
+      input.className = field.className + " pseudo-field";
+      input.id = field.id + "-editable";
+      input.addEventListener('keyup', function (e) {
+        changeField(input.id, field.id)
+      });
+
+      field.replaceWith(input);
     });
-    documentEditable.replaceChild(input, field);
-  });
-}
+  }
+};
